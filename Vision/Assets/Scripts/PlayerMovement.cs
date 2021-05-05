2a552fed14f302dd7753f21b6a64e45c2a8bb5a9
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2021-05-05 17:26:38 +0100

    Player Has Animation
    That should be all the art based stuff done now

--- Vision/Assets/Scripts/PlayerMovement.cs
+++ Vision/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float moveSpeed = 5f;
     public Rigidbody2D RB;
+    public Animator animator;
     private Vector2 movement;
 
     // Update is called once per frame
@@ -15,6 +16,10 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
 
     }
 
